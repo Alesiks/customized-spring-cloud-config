fe05c56173db871eb54cb0652dcae2cb0ea61b94
Ales_Zhuk <Ales_Zhuk@epam.com>
2020-04-17 10:28:27 +0300

    init

---
+++ src/main/java/com/epam/eco/main/Application.java
@@ -0,0 +1,15 @@
+package com.epam.eco.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+
+}
